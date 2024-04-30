617f30d8a789bc738fff9d5cc5574e47f698c3c2
nayaradias1 <nayara_dias95@hotmail.com>
2024-04-29 22:07:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header.jsx'
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+export default function header() {
+  return (
+    <div className='topo'>
+      <h1>Nayara Dias</h1>
+      
+    </div>
+  )
+
+}
