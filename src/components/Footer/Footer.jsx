6873206866b1562e4ec9fbbe1e01d01aca7c2bc3
nayaradias1 <nayara_dias95@hotmail.com>
2024-04-30 12:55:56 -0300

    alterações novas

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import './Footer.css'
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='Footer'>
+      <h3>© Nayara Dias 2023 - 2024</h3>
+    </div>
   )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import './Main.css'
 
 export default function Main() {
   return (
-    <div>Main</div>
+   <>
+   <div>
+    <h5>🚀 Desenvolvedora extremamente Apaixonada por Inovação e Soluções Tecnológicas
+Sou uma profissional apaixonada por transformar ideias inovadoras em soluções tecnológicas concretas. Com uma sólida experiência em desenvolvimento de software e um olhar atento às últimas tendências tecnológicas, estou sempre em busca de desafios que me permitam aprimorar minhas habilidades e contribuir para projetos que fazem a diferença. Estou cursando Engenharia de software pela universidade metropolitana de franca sp a ser concluida em dezembro de 2023.
+#DesenvolvimentoDeSoftware #InovaçãoTecnológica #TransformaçãoDigital #TrabalhoEmEquipe</h5><br/><br/>
+   </div>
+   <div className='Links'>
+    <h3>Conheça Meus trabalhos</h3><br/>
+      <a href=""><i class="fas fa-github"></i>Github</a><br/>
+      <a href=""><i class="fa fa-linkedin" aria-hidden="true">Linkedin</i></a>
+
+    </div>
+   </>
+    
   )
 }
+
+
